9e6c7d6b3d6e1d11701f3df11daccee9846842d3
Vedant Mandwe <vedantmandwe@Vedants-MacBook-Air.local>
2024-12-30 12:25:06 +0530

    Created some components like header, notes and footer and designed UI for header and notes and footer button too

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { NotesComponent } from "./notes/notes.component";
+import { FooterComponent } from "./footer/footer.component";
+import { NewNoteComponent } from "./new-note/new-note.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, NotesComponent, FooterComponent, NewNoteComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'static-website';
+  
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { NewNoteComponent } from './new-note/new-note.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path : 'add-note',
+        component : NewNoteComponent
+    }
+];
